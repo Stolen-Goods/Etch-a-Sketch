f59bfd0ce38d109d8a8cc9db6dbf16545ae7c2e7
Matt <hurley04155@gmail.com>
2024-09-15 19:08:59 -0400

    Add drawing effect

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-const mainContainer = document.getElementById("main-container");
-
 function grid() {
     for (let i = 0; i < 256; i++) {
+        const mainContainer = document.getElementById("main-container");
         const square = document.createElement("div");
-        square.className = "square";
+        square.classList = "square";
         mainContainer.append(square);
     }
+    const artWork = document.querySelectorAll(".square");
+    for (const art of artWork) {
+    art.addEventListener("mouseover", () => {
+        art.classList.add("paint");
+    })
+}
 }
 
+
+
+
 grid();
+
